198dad195e28a28cea7fe8d8e9ceb06d2e30d2c6
Jose Guadalupe Barron Tovar <jose.barron@neoris.com>
2022-06-27 13:02:49 -0500

    s

---
+++ Core/Entities/Marca.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Core.Entities
+{
+    public class Marca : BaseEntity
+    {
+        public string? Nombre { get; set; }
+        public ICollection<Producto>? Productos { get; set; }
+    }
+}
---
+++ Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,68 @@
+﻿using Core.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork,IDisposable
+    {
+        private readonly TiendaContext _context;
+        private IProductoRepository _productos;
+        private IMarcaRepository _marcas;
+        private ICategoriaRepository _categoria;
+        public UnitOfWork(TiendaContext context)
+        {
+            _context = context;
+        }
+        public IProductoRepository Productos
+        {
+            get
+            {
+                if (_productos is null)
+                {
+                    _productos = new ProductRepository(_context);
+                }
+                return _productos;
+            }
+        }
+
+        public IMarcaRepository Marca
+        {
+            get
+            {
+                if (_marcas is null)
+                {
+                    _marcas = new MarcaRepository(_context);
+                }
+                return _marcas;
+            }
+        }
+
+        public ICategoriaRepository Categoria
+        {
+            get
+            {
+                if(_categoria is null)
+                {
+                    _categoria = new CategoriaRepository(_context);
+                }
+                return _categoria;
+            }
+        }
+
+        public async Task<int> SaveAsync()
+        {
+           return await _context.SaveChangesAsync();
+        }
+       
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
