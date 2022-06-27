aa7b673f2c6f17add57c4288f809ff4579538673
Jose Guadalupe Barron Tovar <jose.barron@neoris.com>
2022-06-27 11:17:38 -0500

    s

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Core.Interfaces;
+using Infrastructure.Repositories;
+using Infrastructure.UnitOfWork;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection service) =>
+            service.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+
+        public static void AddApplicationService(this IServiceCollection service)
+        {
+            //service.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            //service.AddScoped<IProductoRepository, ProductRepository>();
+            //service.AddScoped<IMarcaRepository, MarcaRepository>();
+            //service.AddScoped<ICategoriaRepository, CategoriaRepository>();
+            service.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
---
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductRepository : GenericRepository<Producto>, IProductoRepository
+    {
+        //protected readonly TiendaContext _context;
+        public ProductRepository(TiendaContext context):base(context)
+        {
+           // _context = context;
+
+        }
+        public async Task<IEnumerable<Producto>> GetProductosMasCaros(int cantidad)=>
+            await _context.Productos.OrderByDescending(_ => _.Precio).Take(cantidad).ToListAsync();
+           
+        public override async Task<Producto> GetByIdAsync(int id)
+        {
+            return await _context.Productos
+                                 .Include(_ => _.Marca)
+                                 .Include(_ => _.Categoria)
+                                 .FirstOrDefaultAsync(_=>_.Id==id);
+        }
+
+        public override async Task<IEnumerable<Producto>> GetAllAsync()
+        {
+            return await _context.Productos
+                                 .Include(_ => _.Marca)
+                                 .Include(_ => _.Categoria)
+                                 .ToListAsync();
+        }
+    }
+}
