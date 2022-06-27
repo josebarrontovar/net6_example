30f466d81ce32f8bf247334f291fda69046d8aca
Jose Guadalupe Barron Tovar <jose.barron@neoris.com>
2022-06-27 13:02:44 -0500

    s

--- Core/Entities/Categoria.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Core.Entities
-{
-    public class Categoria : BaseEntity
-    {
-        public string? Nombre { get; set; }
-        public ICollection<Producto>? Productos { get; set; }
-    }
-}
