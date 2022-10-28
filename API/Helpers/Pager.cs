201dad16752f826e887ab883b285176d532f18f4
Jose Guadalupe Barron Tovar <jose.barron@neoris.com>
2022-10-28 13:04:59 -0500

    Cambio2

--- API/Helpers/Pager.cs
+++ API/Helpers/Pager.cs
@@ -5,7 +5,7 @@
         public string Search { get; private set; }
         public int PageIndex { get; private set; }
         public int PageSize { get; private set; }
-        public int Total { get; private set; }
+        public int Totals { get; private set; }
         public IEnumerable<T> Registers { get; private set; }
 
         public Pager(IEnumerable<T> registers, int total,int pageIndex, int pageSize, string search)
